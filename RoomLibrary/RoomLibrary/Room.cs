09e460707974058f1e0ecda5b5948bac6963c496
Arina <118733508+arinachernova1@users.noreply.github.com>
2023-04-24 10:04:53 +0300

    Add files via upload

---
+++ RoomLibrary/RoomLibrary/Room.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth = value; }
+        }
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLength + roomWidth);
+
+        }
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+        public string Info()
+        {
+            return "Комната площадью" + RoomArea() + "кв.м";
+        }
+    }
+    public class LivingRoom : Room
+    { int numWin;
+    public int NumWin
+        { get { return numWin; } set { numWin = value;} }
+        public string Info()
+        {
+            return "Жилая комната площадью" + RoomArea() + "кв.м, с" + numWin + "окнами";
+        }
+    }
+    public class Office : Room
+    {
+        int numSockets
+        { get { return numSockets; } set { numSockets = value; } }
+
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+        public string Info()
+        {
+            return "Офис на" + NumWorkplaces() + "рабочих мест";
+        }
+    } 
+
+}
